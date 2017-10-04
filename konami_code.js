fd9e65ec4a66030be42f70f33ae21a04296755ad
Franklyn Thompson <frankistone28@outlook.com>
2017-10-04 18:22:11 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,25 +4,7 @@ var body = document.querySelector("body");
 var index = 0;
 
 function init() {
-    body.addEventListener("keydown", function(e){
-      function onKeyDownHandler(e) {
-        const key = parseInt(e.detail || e.which);
-
-        if (key === code[index]) {
-          index++;
-
-          if (index === code.length) {
-            alert("Hurray!");
-
-            index = 0;
-          }
-        } else {
-          index = 0;
-        }
-      }
-
-      onKeyDownHandler(e);
-    });
+    
 }
 
 init();
